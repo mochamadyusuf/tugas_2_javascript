7b5bc138c8878f0fd02edf3ff8723806f76f28cc
mochamadyusuf <mochamadd.yusuf@gmail.com>
2022-04-08 17:17:25 +0700

    tugas 2 javascript niomic

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+// tipe data number
+let angka = 11;
+
+// tipe data string
+let kata = "yusuf";
+
+// tipe data boolean
+let pilih = true;
+
+// tipe data null dan undifined
+let kosong = null;
+let tidakDiketahui;
+
+// tipe data array
+let kumpulan = [2, 4, 6, 8];
+
+console.log(angka);
+console.log(kata);
+console.log(pilih);
+console.log(kosong);
+console.log(kumpulan);
